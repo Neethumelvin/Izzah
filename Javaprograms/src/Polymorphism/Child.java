454dd7b27630c0e8f874b1caa301ae08c727c898
lenovo <lenovo@DESKTOP-O6BEVCH>
2023-07-10 22:07:47 +0530

    Polymorphism

---
+++ Javaprograms/src/Polymorphism/Child.java
@@ -0,0 +1,22 @@
+package Polymorphism;
+
+public class Child extends Parent{
+	public int operation(int x, int y) {
+		super.operation(2, 3);
+		int result= x*y;
+		System.out.println("Result:"+result);
+		System.out.println("Result:"+super.result);
+		return result;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Child ch=new Child();
+		ch.operation(10, 20);
+		Parent pa=new Parent();
+		pa.operation(13, 30);
+		pa.display();
+	}
+
+}
---
+++ Javaprograms/src/Polymorphism/Parent.java
@@ -0,0 +1,18 @@
+package Polymorphism;
+
+public class Parent {
+	int result;
+	public int operation(int x, int y) {
+		result=x+y;
+		return result;
+	}
+public void display()
+{
+	System.out.println("result:"+result);
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
